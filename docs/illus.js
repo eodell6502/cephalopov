1cc6d84a0b84026841fb4e5f4dd0c98eac69a519
Eric O'Dell <eocom@kyrillia.com>
2018-11-07 05:56:00 -0600

    C1: Doc work

---
+++ docs/illus.js
@@ -0,0 +1,12 @@
+//==============================================================================
+// This CephaloPOV program generates the POV-Ray scripts for producing the
+// illustrations for the documentation.
+//==============================================================================
+
+
+function main(cpov) {
+
+
+}
+
+module.exports = main;
