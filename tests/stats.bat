a6b3d4e36bed11602b011ec9ec0fb6bb45c6ba6c
Eric O'Dell <eocom@kyrillia.com>
2018-11-22 15:06:36 -0500

    C1: Updated stats script, added xset docs

--- tests/stats.bat
+++ tests/stats.bat
@@ -1,3 +1,3 @@
 cd ..
-sloc cephalopov.js classes.js codegen.js colorsInc.js cpov.js file.js gsDef.js ioDef.js miscDef.js objDef.js primitiveDef.js snippets.js vectorDef.js tests\test.js
+sloc cephalopov.js classes.js codegen.js colorsInc.js cpov.js file.js gsDef.js ioDef.js miscDef.js objDef.js primitiveDef.js snippets.js vectorDef.js tests\test.js docs\illus.js
 cd tests
