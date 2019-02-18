85e0b0520f85cd0fe45a2ef23a32b1c700957fe6
eodell <eocom@kyrillia.com>
2019-02-17 22:05:42 -0600

    Moved tests and examples to its own repo; installation work.

--- tests/stats.bat
+++
@@ -1,3 +0,0 @@
-cd ..
-sloc cephalopov.js classes.js codegen.js colorsInc.js cpov.js file.js gsDef.js ioDef.js miscDef.js objDef.js primitiveDef.js snippets.js vectorDef.js tests\test.js docs\illus.js
-cd tests
--- tests/test_home.bat
+++
@@ -1,4 +0,0 @@
-cd ..
-node codegen -c
-cd tests
-node ..\cpov.js -dd -i d:\prog\CephaloPOV\tests\test.js -p d:\prog\CephaloPOV\tests\preamble1.sdl -p d:\prog\CephaloPOV\tests\preamble2.sdl -s colors.inc -s glass.inc
